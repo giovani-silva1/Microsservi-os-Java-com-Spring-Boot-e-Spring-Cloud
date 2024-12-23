7bfa24e415688fdb53d4a6a427633959863d96b6
Giovani Silva <giovani-silva1@live.com>
2024-12-22 23:15:47 -0300

    criado o projeto hr-payroll e implementacao do calculo total e services + resources

---
+++ hr-payroll/src/main/java/com/giovanidev/hrpayroll/services/PaymentService.java
@@ -0,0 +1,17 @@
+package com.giovanidev.hrpayroll.services;
+
+import org.springframework.stereotype.Service;
+
+import com.giovanidev.hrpayroll.entity.Payment;
+
+@Service
+public class PaymentService {
+	
+	
+	public Payment getPayment(long workerId,int days) {
+		return new Payment("Bob",200.0,days);
+	}
+	
+	
+
+}
