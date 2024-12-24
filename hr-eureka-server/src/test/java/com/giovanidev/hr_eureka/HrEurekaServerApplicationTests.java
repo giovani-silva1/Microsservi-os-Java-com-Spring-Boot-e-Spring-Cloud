b9245959c62c282403a972eca9c9aecf8824958a
Giovani Silva <giovani-silva1@live.com>
2024-12-23 23:16:06 -0300

    criado o projeto Eureka-server

---
+++ hr-eureka-server/src/test/java/com/giovanidev/hr_eureka/HrEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.giovanidev.hr_eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
